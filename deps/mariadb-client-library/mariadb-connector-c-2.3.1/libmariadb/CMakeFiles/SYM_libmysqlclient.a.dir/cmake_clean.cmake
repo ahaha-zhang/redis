36d2750ac581788f1f89746c87f6573b6b6461ef
zhangyanjun <zhangyanjun@ctrip.com>
2019-03-21 20:34:01 +0800

    add mariadb-client-library

--- deps/mariadb-client-library/mariadb-connector-c-2.3.1/libmariadb/CMakeFiles/SYM_libmysqlclient.a.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/SYM_libmysqlclient.a"
-  "libmysqlclient.a"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/SYM_libmysqlclient.a.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- deps/mariadb-client-library/mariadb-connector-c-3.0.2-src/libmariadb/CMakeFiles/mariadbclient.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "libmariadbclient.pdb"
-  "libmariadbclient.a"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang C)
-  include(CMakeFiles/mariadbclient.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
